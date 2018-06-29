368f6af65277485241235e916195e9e7aeb98d62
jakabadambalazs <jakabadambalazs@gmail.com>
2018-06-29 12:14:12 +0200

    TC - integrated Fullcalendar library - displaying empty

--- modules/custom/training_calendar/js/training-calendar.js
+++ modules/custom/training_calendar/js/training-calendar.js
@@ -54,9 +54,22 @@
     Drupal.behaviors.trainingCalendar = {
         attach: function attach(context, settings) {
             console.log("TRAINING CALENDAR...");
+
+
+            /*
             let TrainingCalendarApp = new Drupal.trainingCalendar.TrainingList({
                 collection: new Drupal.trainingCalendar.TrainingModels
             });
+            */
+
+            $('#training-calendar').fullCalendar({
+                weekends: true,
+                defaultView: 'month',
+                showNonCurrentDates: true,
+                weekNumbers: true,
+            });
+
+
         }
     };
 
