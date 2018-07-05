a03b4716a94cb5a9436e3e60f47411d6f70333a5
AdiBadi <jakabadambalazs@gmail.com>
2018-07-05 22:56:44 +0200

    TC - implementing a custom view for fullcalendar

--- modules/custom/training_calendar/js/utilities/view-manager.js
+++ modules/custom/training_calendar/js/utilities/view-manager.js
@@ -35,11 +35,32 @@
         setupCalendar: function()
         {
             $trainingCalendarDiv.fullCalendar({
+                height: "auto",
                 weekends: true,
-                defaultView: 'month',
+                firstDay: 1,
                 showNonCurrentDates: true,
                 weekNumbers: true,
                 events: Drupal.trainingCalendar.Utilities.ModelManager.getCalendarEvents,
+                defaultView: 'month',
+                header: {
+                    left: 'month training_calendar_view newTrainingButton',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                customButtons: {
+                    newTrainingButton: {
+                        text: '+Training',
+                        click: function() {
+                            alert('Adding new training!');
+                        }
+                    }
+                },
+                eventClick: function(calEvent, jsEvent, view) {
+                    alert('Event: ' + calEvent.title);
+                    //alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+                    //alert('View: ' + view.name);
+                    //$(this).css('border-color', 'red');
+                }
             });
         },
 
@@ -53,4 +74,25 @@
             $overlayDiv.delay(500).fadeIn(500);
         }
     };
+
+    let FC = $.fullCalendar;
+    let FCView = FC.View;
+    let FCBasicView = FC.BasicView;
+    let FCMonthView = FC.MonthView;
+
+    let TrainingCalendarView = FCMonthView.extend({
+
+        _initialize: function() {
+            console.log("CUSTOM VIEW INIT!");
+
+
+            //FCView.prototype.initialize.apply(this, arguments);
+        },
+
+    });
+
+    FC.views.training_calendar_view = TrainingCalendarView; // register our class with the view system
+
+
+
 })(Drupal, jQuery, _);
